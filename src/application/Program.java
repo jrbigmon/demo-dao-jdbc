1b558cddb59232aceb7289a04ff7a736fc959661
jrbigmon <juniorterriaga0@gmail.com>
2024-02-17 15:37:48 -0300

    feat: seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        Department dp = new Department(1, "Salles");
+        Department dp = new Department(1, "Sales");
+        Seller seller = new Seller(1, "vagner", "vagner.junior@email", new Date(), 3000.00, dp);
 
         System.out.println(dp);
+        System.out.println(seller);
     }
 }
