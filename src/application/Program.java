30195948203e87b689d1919aa756bebb159db2b7
jrbigmon <juniorterriaga0@gmail.com>
2024-02-17 14:41:51 -0300

    feat: department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import models.entities.Department;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Department dp = new Department(1, "Salles");
+
+        System.out.println(dp);
+    }
+}
