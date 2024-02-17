f3452e7db1f451fadf1af6f1c819b0384f6b3194
jrbigmon <juniorterriaga0@gmail.com>
2024-02-17 15:43:28 -0300

    feat: seller dao interface

---
+++ src/models/dao/SellerDao.java
@@ -0,0 +1,17 @@
+package models.dao;
+
+import java.util.List;
+
+import models.entities.Seller;
+
+public interface SellerDao {
+  void insert(Seller seller);
+
+  void update(Seller seller);
+
+  void deleteById(Integer id);
+
+  Seller findById(Integer id);
+
+  List<Seller> findAll();
+}
