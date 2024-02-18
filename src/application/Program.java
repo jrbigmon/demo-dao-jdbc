5072135d5329417f2578b098c968c8a685a88373
jrbigmon <juniorterriaga0@gmail.com>
2024-02-18 20:00:29 -0300

    test: seller dao find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,15 @@
 package application;
 
-import java.util.Date;
-
-import models.entities.Department;
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
 import models.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        Department dp = new Department(1, "Sales");
-        Seller seller = new Seller(1, "vagner", "vagner.junior@email", new Date(), 3000.00, dp);
+        SellerDao sellerDao = DaoFactory.getSellerDao();
 
-        System.out.println(dp);
+        Seller seller = sellerDao.findById(31);
+
         System.out.println(seller);
     }
 }
